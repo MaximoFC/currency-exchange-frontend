28cefb9d089ec24453ec244368a85cb2c7057ec9
MaximoFC <maximofcallejas@gmail.com>
2025-03-26 21:57:29 -0300

    Navbar h2 modified

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,8 @@ import '../index.css';
 
 const Navbar = () => {
     return (
-        <nav className='text-black p-6 flex justify-between border-b-1 border-gray-300'>
-            <h2>Casa de cambio</h2>
+        <nav className='text-black p-6 flex justify-between border-b-1 border-gray-300 align-center'>
+            <h2 className='text-xl'>Casa de cambio</h2>
             <div className='flex justify-between w-70'>
                 <Link to="/" className='hover:text-gray-400 transition'>Home</Link>
                 <Link to="/clients" className='hover:text-gray-400 transition'>Clientes</Link>
